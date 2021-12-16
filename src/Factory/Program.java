cb30812ed89c1188bc12a47364810354b4334d0c
Gheorghi-1 <gheorghi.verdes@gmail.com>
2021-12-16 03:14:17 +0200

    Factory exemplul 3

--- src/Factory/Program.java
+++ src/Factory/Program.java
@@ -2,7 +2,7 @@ package Factory;
 
 public class Program {
     public static void main(String[] args) {
-       DeveloperFactory developerFactory = new JavaDeveloperFactory(); //schimbam JavaDeveloperFactory pe CppDeveloperFactory
+       DeveloperFactory developerFactory = new JavaDeveloperFactory(); //schimbam fabrica JavaDeveloperFactory pe CppDeveloperFactory
        Developer developer = developerFactory.createDeveloper();
        //si chemam metoda writecode
         developer.writeCode();
