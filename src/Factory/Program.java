0dae332599a5bfbfd7460f192795c8d2d3e91d93
Gheorghi-1 <gheorghi.verdes@gmail.com>
2021-12-16 03:42:36 +0200

    Factory exemplul 3.1

--- src/Factory/Program.java
+++ src/Factory/Program.java
@@ -2,9 +2,20 @@ package Factory;
 
 public class Program {
     public static void main(String[] args) {
-       DeveloperFactory developerFactory = new JavaDeveloperFactory(); //schimbam fabrica JavaDeveloperFactory pe CppDeveloperFactory
+       DeveloperFactory developerFactory = createDeveloperBySpecealty("ja"); //schimbam fabrica JavaDeveloperFactory pe CppDeveloperFactory
        Developer developer = developerFactory.createDeveloper();
        //si chemam metoda writecode
         developer.writeCode();
     }
+
+        static DeveloperFactory createDeveloperBySpecealty(String specialty){
+        if(specialty.equalsIgnoreCase("java")){
+            return new JavaDeveloperFactory();
+        }else if(specialty.equalsIgnoreCase("cpp")){
+            return new CppDeveloperFactory();
+        }else {
+            throw new RuntimeException("ATENTIE! Clasa " +specialty +" nu exista");
+        }
+        }
+
 }
